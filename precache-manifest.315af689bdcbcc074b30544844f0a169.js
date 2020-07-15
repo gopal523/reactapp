9c20ef1b867252993af863eb60635a8cf09e94cf
Gopal <gopalkumaryahoo@gmail.com>
2020-07-15 12:47:00 +0530

    Updates

---
+++ precache-manifest.315af689bdcbcc074b30544844f0a169.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99ce6753bb37c21bc93c996d31123fe",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "48c148b751b244815a19",
+    "url": "/reactapp/static/css/main.0b625bae.chunk.css"
+  },
+  {
+    "revision": "337cf1e6a07cc2715fe6",
+    "url": "/reactapp/static/js/2.703f4184.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactapp/static/js/2.703f4184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48c148b751b244815a19",
+    "url": "/reactapp/static/js/main.623bdc99.chunk.js"
+  },
+  {
+    "revision": "e219a2030a13ac34f0be",
+    "url": "/reactapp/static/js/runtime-main.f935dd26.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
